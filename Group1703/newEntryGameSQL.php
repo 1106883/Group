2dcd07f811059cde8442ec4672c17f0dd37fa179
1106883 <1106883@rgu.ac.uk>
2016-04-11 12:38:50 +0100

    hi

--- Group1703/newEntryGameSQL.php
+++ Group1703/newEntryGameSQL.php
@@ -8,7 +8,7 @@ error_reporting(-1);
 $dsn = "mysql:host=eu-cdbr-azure-north-d.cloudapp.net;dbname=db1510646_gameshare";
 $username = "b52b6c6935c6d2";
 $password = "26ebeed0";
-try {
+
     $conn = new PDO($dsn, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -35,9 +35,7 @@ try {
     header("Location:AddCopy.php");
 
 
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
+
 
 $conn = null;
 ?>
