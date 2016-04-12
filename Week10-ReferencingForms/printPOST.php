581ac0ad516a492ab2cead37afedb443d4566c05
1106883 <1106883@rgu.ac.uk>
2016-04-12 09:48:24 +0100

    hi

--- Week10-ReferencingForms/printPOST.php
+++ Week10-ReferencingForms/printPOST.php
@@ -3,4 +3,4 @@ $title=$_POST['Title'];
 $content=$_POST['Content'];
 $category=$_POST['Category'];
 
-echo "<article><h1>$title</h1><br><p>$content</p><br><strong>$category</strong>";
+echo "<article><h1>$title</h1><br><p>$content</p><br><strong>$category</strong></article>";
