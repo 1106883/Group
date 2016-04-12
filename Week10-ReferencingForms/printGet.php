e09f3af81ee345905940814fdebde152948e46ee
1106883 <1106883@rgu.ac.uk>
2016-04-12 09:40:58 +0100

    hi

--- Week10-ReferencingForms/printGet.php
+++ Week10-ReferencingForms/printGet.php
@@ -3,4 +3,4 @@
 $pet=$_GET['pet'];
 $type=$_GET['type'];
 
-echo "<ul><li>'$pet'</li><li>'$type'</li>";
+echo "<ul><li>$pet</li><li>$type</li>";
---
+++ Week10-ReferencingForms/printPOST.php
@@ -0,0 +1,6 @@
+<?php
+$title=$_POST['Title'];
+$content=$_POST['Content'];
+$category=$_POST['Category'];
+
+echo "<article><h1>$title</h1><br><p>$content</p><br><strong>$category</strong>";
