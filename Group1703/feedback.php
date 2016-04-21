d81750c834d4bb1e05852ea53c428bd27137e9b3
1106883 <1106883@rgu.ac.uk>
2016-04-21 11:12:51 +0100

    hi

--- Group1703/feedback.php
+++ Group1703/feedback.php
@@ -1,4 +1,7 @@
-<?
+<?session_start();
+if(!isset($_SESSION['username'])){
+    header("Location:home.php");
+}
 $dsn = "mysql:host=eu-cdbr-azure-north-d.cloudapp.net;dbname=db1510646_gameshare";
 $username = "b52b6c6935c6d2";
 $password = "26ebeed0";
@@ -12,11 +15,12 @@ $bID = $_GET['feedback'];
 $feedback = $_GET['feedbackRating'];
 
 
-$query = "Insert into feedback (borrowID, borrowerID, feedbackScore) Values ()";
+$query = "Select borrowerID FROM borrow WHERE borrow.borrowID = '$bID' Insert into feedback (borrowID, borrowerID, feedbackScore) Values ('$bID', borrowerID, '$feedback')";
 try {
     $results = $conn->query($query);
 
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
+header("Location:home.php");
 ?>
