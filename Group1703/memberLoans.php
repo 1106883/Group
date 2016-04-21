a2066065faeb34936f409bcbdefb8c2c7d81f7b4
1106883 <1106883@rgu.ac.uk>
2016-04-21 10:46:19 +0100

    hi

--- Group1703/memberLoans.php
+++ Group1703/memberLoans.php
@@ -56,7 +56,7 @@ if(!isset($_SESSION['username'])){
                 echo "Connection failed: " . $e->getMessage();
             }
 
-            $query = "SELECT loanerID, copyID, Title, start_date, end_date FROM borrow INNER JOIN gamecollection ON borrow.gameID = gamecollection.gameID WHERE borrowerID = ".$_SESSION["username"]." ORDER BY end_date";
+            $query = "SELECT loanerID, copyID, Title, start_date, end_date FROM borrow INNER JOIN gamecollection ON borrow.gameID = gamecollection.gameID WHERE borrowerID = ".$_SESSION["username"]." AND Now() < end_date ORDER BY end_date";
             try {
                 $results = $conn->query($query);
 
